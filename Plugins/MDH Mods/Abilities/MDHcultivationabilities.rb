2c9c85eb4be79213eea57ec6b67e0f54bd4d521d
Mfalmelayo <mfalmelayo@gmail.com>
2026-01-26 14:27:43 -0600

    Gardevoir new ability set up
    Gardevoir is getting a new ablity that I'm working.

--- Plugins/MDH Mods/Abilities/MDHcultivationabilities.rb
+++ Plugins/MDH Mods/Abilities/MDHcultivationabilities.rb
@@ -7,6 +7,26 @@ Battle::AbilityEffects::DamageCalcFromUser.add(:FIVEMAGICS,
   proc { |ability, user, target, move, mults, power, type|
     next if !move.damagingMove?
     next unless [:POISON, :PSYCHIC, :GHOST, :FIRE, :ELECTRIC].include?(type)
+    battle.pbShowAbilitySplash(user)
+	mults[:final_damage_multiplier] *= 1.2
+	battle.pbDisplay(_INTL("{1} has mastered {2}", user.pbThis))
+	battle.pbHideAbilitySplash(user)
+  }
+)
+
+# 1.2x damage for the user's Psychic and fairy moves
+Battle::AbilityEffects::DamageCalcFromUser.add(:TRUEEMBRACE,
+  proc { |ability, user, target, move, mults, power, type|
+    next if !move.damagingMove?
+    next unless [:PSYCHIC, :FAIRY].include?(type)
     mults[:final_damage_multiplier] *= 1.2
+  }
+)
+
+# 10% damage resistance 
+Battle::AbilityEffects::DamageCalcFromTarget.add(:TRUEEMBRACE,
+  proc { |ability, user, target, move, mults, power, type|
+    next unless move.damagingMove?
+    mults[:final_damage_multiplier] *= 0.9
   }
 )
