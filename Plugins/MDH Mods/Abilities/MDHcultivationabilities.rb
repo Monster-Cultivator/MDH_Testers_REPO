ab070ab3d9b18f1d1eb710a97e06886d1d19ef5d
Mfalmelayo <mfalmelayo@gmail.com>
2026-01-27 18:01:47 -0600

    new abilities and pokemon changes!
    Some people got buffed and finally got their innates! Also Hybrid Ninetales got a new ability.

--- Plugins/MDH Mods/Abilities/MDHcultivationabilities.rb
+++ Plugins/MDH Mods/Abilities/MDHcultivationabilities.rb
@@ -14,6 +14,11 @@ Battle::AbilityEffects::DamageCalcFromUser.add(:FIVEMAGICS,
   }
 )
 
+#===============================================================================
+# Ability: TRUEEMBRACE
+# Powers up fairy & psychic type moves, also gives a 10% damage reduction.
+#===============================================================================
+
 # 1.2x damage for the user's Psychic and fairy moves
 Battle::AbilityEffects::DamageCalcFromUser.add(:TRUEEMBRACE,
   proc { |ability, user, target, move, mults, power, type|
@@ -28,5 +33,24 @@ Battle::AbilityEffects::DamageCalcFromTarget.add(:TRUEEMBRACE,
   proc { |ability, user, target, move, mults, power, type|
     next unless move.damagingMove?
     mults[:final_damage_multiplier] *= 0.9
+  }
+)
+
+#===============================================================================
+# Ability: KITSUNECROSS
+# Powers up fairy & psychic type moves, also gives a 10% damage reduction.
+#===============================================================================
+
+Battle::AbilityEffects::AccuracyCalcFromTarget.add(:KITSUNECROSS,
+  proc { |ability, mods, user, target, move, type|
+    mods[:evasion_multiplier] *= 1.25 if target.effectiveWeather == :Sun, :HarshSun
+  }
+)
+
+Battle::AbilityEffects::DamageCalcFromUser.add(:KITSUNECROSS,
+  proc { |ability, user, target, move, mults, power, type|
+    next if !move.damagingMove?
+    next unless [:FIRE, GHOST].include?(type)
+    mults[:final_damage_multiplier] *= 1.2
   }
 )
